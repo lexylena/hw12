ad8c60092a043bf7f24754a00f1d72e56d20830f
tburgerman <tburgerman@gmail.com>
2017-04-17 13:46:33 -0400

    Directory.c
    Made directory.c and initial comments as well as edits to directory.h

--- hints/directory.h
+++ hints/directory.h
@@ -9,6 +9,7 @@
 
 typedef struct dirent {
     char   name[48];
+    int name_len; //length of file name
     int inode_idx; // index in inodes array
 } dirent;
 
@@ -20,10 +21,10 @@ typedef struct directory {
 void directory_init(); // create root directory from already created inode at index 2 (always)
 
 directory directory_from_pnum(int pnum);
-int directory_lookup_pnum(directory dd, const char* name);
+int directory_lookup_idx(directory dd, const char* name);
 int tree_lookup_pnum(const char* path);
 directory directory_from_path(const char* path);
-int directory_put_ent(directory dd, const char* name, int pnum);
+int directory_put_ent(directory dd, const char* name, int idx);
 int directory_delete(directory dd, const char* name);
 slist* directory_list(const char* path);
 void print_directory(directory dd);
